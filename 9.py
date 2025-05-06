2f0147c5668ca06374d17cfce7512f47a4758290
ligiandrz <ligiandrz@gmail.com>
2025-05-06 11:36:35 -0300

    9.py

--- 9.py
+++ 9.py
@@ -1,3 +1,6 @@
+#- Faça um algoritmo que calcule o IMC (Índice de Massa Corporal) de uma
+#pessoa, leia o seu peso e sua altura e imprima na tela sua condição de acordo
+#com a tabela abaixo:
 peso = int(input("insira seu peso:"))
 altura = float(input("insira sua altura:"))
 imc = peso / ((altura / 100)**2)
