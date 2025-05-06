8b85b0e2381b7e12a30b455752038b2ef6068e39
ligiandrz <ligiandrz@gmail.com>
2025-05-06 11:31:53 -0300

    7.py

--- 7.py
+++ 7.py
@@ -1,3 +1,5 @@
+#Faça um algoritmo que leia dois valores booleanos (lógicos) e determine se
+#ambos são VERDADEIRO ou FALSO. Sendo o numero 1 Verdadeiro e 0 Falso.
 valor1 =int(input("digite o primeiro valor(1 para verdadeiro, 0 para falso):"))
 valor2 =int(input("digite o segundo valor(1 para verdadeiro, 0 para falso):"))
 if valor1 == 1 and valor2 == 1:
