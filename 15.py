3b0bccb4909a1dc59be31dcf6e544f707c479dd3
ligiandrz <ligiandrz@gmail.com>
2025-05-06 11:19:58 -0300

    15.py

--- 15.py
+++ 15.py
@@ -1,3 +1,7 @@
+#Faça um algoritmo que leia o ano em que uma pessoa nasceu, imprima na
+#tela quantos anos, meses e dias essa pessoa ja viveu. Leve em
+#consideração o ano com 365 dias e o mês com 30 dias.
+#(Ex: 5 anos, 2 meses e 15 dias de vida)
 ano_nascimento = int(input("digite o ano em que você nasceu: "))
 ano_atual = int(input("digite o ano atual: "))
 anos_vivid = ano_atual - ano_nascimento
