1efe49c9050e75a8a1768a838cc6efba8c434630
ligiandrz <ligiandrz@gmail.com>
2025-05-06 11:14:37 -0300

    10.py

--- 10.py
+++ 10.py
@@ -1,3 +1,4 @@
+#Faça um algoritmo que leia três notas obtidas por um aluno, e imprima na tela a média das notas.
 nota1 = int(input("insira sua nota 1:"))
 nota2 = int(input("insira sua nota 2:"))
 nota3 = int(input("insira sua nota 3:"))
