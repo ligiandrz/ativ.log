09c9728b29d0f6b39773670a7e4c17e011fd139c
ligiandrz <ligiandrz@gmail.com>
2025-05-06 11:30:25 -0300

    5.py

--- 5.py
+++ 5.py
@@ -1,3 +1,6 @@
+#Faça um algoritmo que leia o valor do salário mínimo e o valor do salário de
+#um usuário, calcule quantos salários mínimos esse usuário ganha e imprima na
+#tela o resultado. (Base para o Salário mínimo R$ 1.518,00).
 S = float(input("digite seu salário:"))
 Sminimo =  1.518
 QS = S / 1518
