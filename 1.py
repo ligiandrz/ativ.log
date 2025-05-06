5b6549fdb8a569dd0d46b968cbb1fae61630a2b8
ligiandrz <ligiandrz@gmail.com>
2025-05-06 11:13:50 -0300

    1.py

--- 1.py
+++ 1.py
@@ -1,3 +1,4 @@
+# Faça um algoritmo que leia os valores de A, B, C e em seguida imprima na tela a soma entre A e B é mostre se a soma é menor que C.
 valorA = float(input("insira o valor de A: "))
 valorB = float(input("insira o valor de B: "))
 valorC = float(input("insira o valor de C: "))
