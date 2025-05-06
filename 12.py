b3888d4ef11c2ff8d4157e4ef05103caffc1d2cc
ligiandrz <ligiandrz@gmail.com>
2025-05-06 11:16:48 -0300

    12.py

--- 12.py
+++ 12.py
@@ -1,3 +1,12 @@
+#Faça um algoritmo que leia o valor de um produto e determine o valor que
+#deve ser pago, conforme a escolha da forma de pagamento pelo comprador e
+#imprima na tela o valor final do produto a ser pago. Utilize os códigos da tabela
+#de condições de pagamento para efetuar o cálculo adequado.
+#Tabela de Código de Condições de Pagamento
+#2 - À Vista no cartão de crédito, recebe 10% de desconto
+#3 - Parcelado no cartão em duas vezes, preço normal do produto sem juros
+#4 - Parcelado no cartão em três vezes ou mais, preço normal do produto mais
+#juros de 10%
 valor_produto = float(input("digite o valor do produto: "))
 print("escolha a forma de pagamento:")
 print("a vista em dinheiro ou pix (15% de desconto)")
