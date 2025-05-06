846fd6f43e5666a7d756d7d4077b49383ca249eb
ligiandrz <ligiandrz@gmail.com>
2025-05-06 11:27:18 -0300

    3.py

--- 3.py
+++ 3.py
@@ -1,3 +1,7 @@
+#- Faça um algoritmo que leia dois valores inteiros A e B, se os valores de A e
+#B forem iguais, deverá somar os dois valores, caso contrário devera multiplicar
+#A por B. Ao final de qualquer um dos cálculos deve-se atribuir o resultado a
+#uma variável C e imprimir seu valor na tela.
 valorA =int(input("insira o valor A:"))
 valorB =int(input("insira o valor B;"))
 if valorA == valorB :
