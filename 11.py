c039946d70afc5cb9a8443bfe31121da4b0b662f
ligiandrz <ligiandrz@gmail.com>
2025-05-06 11:15:38 -0300

    11.py

--- 11.py
+++ 11.py
@@ -1,3 +1,6 @@
+#Faça um algoritmo que leia quatro notas obtidas por um aluno, calcule a média das nota obtidas, imprima na tela o nome do aluno e se o aluno foi 
+#aprovado ou reprovado. Para o aluno ser considerado aprovado sua média final
+#deve ser maior ou igual a 7.
 nome = input("insira seu nome:")
 nota1 = int(input("insira sua nota 1:"))
 nota2 = int(input("insira sua nota 2:"))
