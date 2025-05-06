a0e294de38313f1bb45a39e17b889b38aa11fe73
ligiandrz <ligiandrz@gmail.com>
2025-05-06 11:23:30 -0300

    18.py

--- 18.py
+++ 18.py
@@ -1,3 +1,5 @@
+#Francisco tem 1,50m e cresce 2 centímetros por ano, enquanto Sara tem 1,10m e cresce 3 centímetros por ano. Faça um algoritmo que calcule e
+#imprima na tela em quantos anos serão necessários para que Sara seja maior que Francisco.
 altfran = 1.50
 altsara = 1.10
 cfran = 0.02
