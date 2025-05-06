8204dc82cb8447b283701c78ae91cbb4f8eabb48
ligiandrz <ligiandrz@gmail.com>
2025-05-06 11:21:45 -0300

    17.py

--- 17.py
+++ 17.py
@@ -1,3 +1,6 @@
+#Faça um algoritmo que leia três valores que representam os três lados de
+#um triângulo e verifique se são válidos, determine se o triângulo é equilátero,
+#isósceles ou escaleno.
 Far = int(input("insira a temperatura em Fahrenheit"))
 Cel= (5 * ( Far-32) / 9)
 print(f"Fahrenheit= {Far}, Celsius= {Cel:.2f}")
